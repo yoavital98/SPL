ff7366dcf034bc18fd6537d6509afeee0f640a65
yoavital <yoavavi@post.bgu.ac.il>
2021-11-06 19:44:32 +0200

    first init

---
+++ Studio.h
@@ -0,0 +1,28 @@
+#ifndef STUDIO_H_
+#define STUDIO_H_
+
+#include <vector>
+#include <string>
+#include "Workout.h"
+#include "Trainer.h"
+#include "Action.h"
+
+
+class Studio{		
+public:
+	Studio();
+    Studio(const std::string &configFilePath);
+    void start();
+    int getNumOfTrainers() const;
+    Trainer* getTrainer(int tid);
+	const std::vector<BaseAction*>& getActionsLog() const; // Return a reference to the history of actions
+    std::vector<Workout>& getWorkoutOptions();
+
+private:
+    bool open;
+    std::vector<Trainer*> trainers;
+    std::vector<Workout> workout_options;
+    std::vector<BaseAction*> actionsLog;
+};
+
+#endif
---
+++ Trainer.h
@@ -0,0 +1,32 @@
+#ifndef TRAINER_H_
+#define TRAINER_H_
+
+#include <vector>
+#include "Customer.h"
+#include "Workout.h"
+
+typedef std::pair<int, Workout> OrderPair;
+
+class Trainer{
+public:
+    Trainer(int t_capacity);
+    int getCapacity() const;
+    void addCustomer(Customer* customer);
+    void removeCustomer(int id);
+    Customer* getCustomer(int id);
+    std::vector<Customer*>& getCustomers();
+    std::vector<OrderPair>& getOrders();
+    void order(const int customer_id, const std::vector<int> workout_ids, const std::vector<Workout>& workout_options);
+    void openTrainer();
+    void closeTrainer();
+    int getSalary();
+    bool isOpen();
+private:
+    int capacity;
+    bool open;
+    std::vector<Customer*> customersList;
+    std::vector<OrderPair> orderList; //A list of pairs for each order for the trainer - (customer_id, Workout)
+};
+
+
+#endif
