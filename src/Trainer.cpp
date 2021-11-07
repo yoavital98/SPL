e206058e3d14230e283bd29b785aee68f21f4c6a
oranbn <oranbenn@post.bgu.ac.il>
2021-11-07 14:44:06 +0200

    new update

--- src/Trainer.cpp
+++ src/Trainer.cpp
@@ -1,4 +1,37 @@
-//
-// Created by yoavi on 07/11/2021.
-//
+#include "Trainer.h"
 
+class Trainer{
+public:
+    Trainer(int t_capacity) : capacity(t_capacity), open(false), customersList(), orderList() { }
+    int getCapacity() const
+    {
+        return capacity;
+    }
+    void addCustomer(Customer* customer);
+    void removeCustomer(int id);
+    Customer* getCustomer(int id);
+    std::vector<Customer*>& getCustomers();
+    std::vector<OrderPair>& getOrders();
+    void order(const int customer_id, const std::vector<int> workout_ids, const std::vector<Workout>& workout_options);
+    void openTrainer()
+    {
+        open = true;
+    }
+    void closeTrainer()
+    {
+        open = false;
+    }
+    int getSalary();
+    bool isOpen()
+    {
+        return  open;
+    }
+private:
+    int capacity;
+    bool open;
+    std::vector<Customer*> customersList;
+    std::vector<OrderPair> orderList; //A list of pairs for each order for the trainer - (customer_id, Workout)
+};
+
+
+#endif
--- src/Workout.cpp
+++ src/Workout.cpp
@@ -1,4 +1,29 @@
-//
-// Created by yoavi on 07/11/2021.
-//
+#include "Workout.h"
 
+class Workout{
+public:
+    Workout(int w_id, std::string w_name, int w_price, WorkoutType w_type) : id(w_id), name(w_name), price(w_price), type(w_type) { }
+    int getId() const
+    {
+        return id;
+    }
+    std::string getName() const
+    {
+        return name;
+    }
+    int getPrice() const
+    {
+        return price;
+    }
+    WorkoutType getType() const {
+        return type;
+    }
+private:
+    const int id;
+    const std::string name;
+    const int price;
+    const WorkoutType type;
+};
+
+
+#endif
